a51ed1f239b2247d8ca50a805999928c1bec09d4
desconectado <desconectado@DESKTOP-4RVSNCJ>
2016-04-11 01:14:17 +0200

    prueba3

---
+++ proyecto_Gerardo_Rodriguez/src/proyecto_gerardo_rodriguez/Proyecto_Gerardo_Rodriguez.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_gerardo_rodriguez;
+
+/**
+ *
+ * @author desconectado
+ */
+public class Proyecto_Gerardo_Rodriguez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");
+    }
+    
+}
