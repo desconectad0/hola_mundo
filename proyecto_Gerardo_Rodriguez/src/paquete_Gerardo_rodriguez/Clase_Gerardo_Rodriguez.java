88071315a46b9f7e568119e185007c585b95400a
desconectado <desconectado@DESKTOP-4RVSNCJ>
2016-04-11 06:07:15 +0200

    prueba3

--- proyecto_Gerardo_Rodriguez/src/paquete_Gerardo_rodriguez/Clase_Gerardo_Rodriguez.java
+++ proyecto_Gerardo_Rodriguez/src/paquete_Gerardo_rodriguez/Clase_Gerardo_Rodriguez.java
@@ -21,6 +21,7 @@ public class Clase_Gerardo_Rodriguez {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        System.out.println("hola mundo");
     }
     
 }
